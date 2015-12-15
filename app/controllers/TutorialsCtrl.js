9700d0bfa303d232ff334d02b49d82db9f45285b
Roman Kacharaba <tikerzxc@gmail.com>
2015-12-15 18:29:14 +0200

    tutorials-start-fix

--- app/controllers/TutorialsCtrl.js
+++ app/controllers/TutorialsCtrl.js
@@ -7,15 +7,16 @@
     function TutorialsCtrl($scope, getData) {
         $scope.pageId = 'Home';
 
-        //$scope.dataItems = [
-        //    [
-        //        {
-        //            shortDesc : '<b>Tutorials</b>',
-        //            fullDesc: '<br>Je hebt een nieuwe camera gekocht en staat te popelen om veel mooie fotos te gaan maken. Je kijkt naar je camera en ziet allerlei knoppen en menus. Waar moet je beginnen? En wat gebeurt er eigenlijk binnen in de camera en waarom?<br>Wanneer jij de ontspanknop indrukt, vangt de camera licht en legt dit vast op de beeldsensor. Je wilt uiteraard een kwalitatief goede foto, eentje die juist is belicht, dat wil zeggen, niet te licht (overbelicht) en niet te donker (onderbelicht).<br><br>We gaan je uitleggen hoe dit werkt en hoe jij kunt bepalen hoe jouw fotos er uiteindelijk uitzien. We beginnen daarom bij het begin: de belichting.<br><img class="inlineimage" src="../assets/img/elem/programma.jpg"><br><br><br><a class="text-link">Klik hier</a> om met meteen met de theorie te beginnen of klik op <b>Theorie</b>  in de balk hierboven en maak een keuze uit een van de onderwerpen.',
-        //            type: null
-        //        }
-        //    ]
-        //];
+        $scope.dataItems = [
+            [
+                {
+                    shortDesc : '<b>Tutorials</b>',
+                    fullDesc: '<br>Je hebt een nieuwe camera gekocht en staat te popelen om veel mooie fotos te gaan maken. Je kijkt naar je camera en ziet allerlei knoppen en menus. Waar moet je beginnen? En wat gebeurt er eigenlijk binnen in de camera en waarom?<br>Wanneer jij de ontspanknop indrukt, vangt de camera licht en legt dit vast op de beeldsensor. Je wilt uiteraard een kwalitatief goede foto, eentje die juist is belicht, dat wil zeggen, niet te licht (overbelicht) en niet te donker (onderbelicht).<br><br>We gaan je uitleggen hoe dit werkt en hoe jij kunt bepalen hoe jouw fotos er uiteindelijk uitzien. We beginnen daarom bij het begin: de belichting.<br><img class="inlineimage" src="../assets/img/elem/programma.jpg"><br><br><br><a class="text-link">Klik hier</a> om met meteen met de theorie te beginnen of klik op <b>Theorie</b>  in de balk hierboven en maak een keuze uit een van de onderwerpen.',
+                    type: null
+                },
+            ],
+            
+        ];
 
         $scope.viewedSlides = $scope.dataItems = [];
 
--- tutorials/tutorials-short-desc/_posts/2015-01-30-frontpage.markdown
+++ tutorials/tutorials-short-desc/_posts/2015-01-30-frontpage.markdown
@@ -11,11 +11,11 @@ onderwerpen basis begrippen (Diafragma, sluitertijd & ISO), de
 juiste belichting en beweging fotograferen.
 
 Per onderwerp zie je links naar twee video's:
-<ul class="default">
+<ul class="tutorials default">
     <li>een instructievideo die leert over welke knoppen op je 
     camera je kunt gebruiken</li>
     <li>een toepassingsvideo die laat zien je het geleerde in de 
-    praktijk kunt toepassen. <li/>
+    praktijk kunt toepassen. </li>
 </ul>
 
 Klik op <span class="accent">Verder</span> om naar het eerste onderwerp te gaan of klik op 
